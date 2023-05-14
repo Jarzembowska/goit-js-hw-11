dfc7e0c884437b060a6e7a56941933fc8e8048dd
Anna Maria Jarzembowska <jarzembowska.anma@gmail.com>
2023-05-15 01:38:52 +0200

    second commit

---
+++ src/index.js
@@ -0,0 +1,193 @@
+'use strict';
+const userKey = '36375407-b186fdf61b14328b3f73ebdf3';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { throttle } from 'throttle-debounce';
+
+const loadMoreBtn = document.querySelector('.load-more');
+const searchInput = document.querySelector('form input');
+const searchBtn = document.querySelector('form button');
+const loadMoreDiv = document.querySelector('.load-more-footer');
+const scrollTopBtn = document.querySelector('.scrollTopButton');
+const gallery = document.querySelector('.gallery');
+const loadFormBtn = document.querySelector('.loadModeBtn');
+
+let loadMoreVisiblecheck = false;
+let autoScrollFlag = true;
+
+//
+let galleryItemsLimit = 0;
+let galleryItemsRendered = 0;
+let pageCounter = 1;
+let numbersOfPagesToRender = 0;
+let simpleGallery = new SimpleLightbox('.gallery a');
+let isFirstSearch = true;
+scrollTopBtn.style.display = 'none';
+
+//
+
+const loadMoreVisible = value => {
+  if (value && loadMoreVisiblecheck && searchInput.value != '') {
+    loadMoreBtn.style.display = 'block';
+    loadMoreDiv.style.display = 'flex';
+  } else {
+    loadMoreBtn.style.display = 'none';
+    loadMoreDiv.style.display = 'none';
+  }
+};
+loadMoreVisible(false);
+
+searchBtn.addEventListener('click', event => {
+  event.preventDefault();
+  newSearch(searchInput.value);
+});
+
+const fetchImages = async searchString => {
+  try {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=${userKey}&q=${searchString}&image_type=photo$orientation=horizontal&safesearch=true?fields=webformatURL,largeImageURL,tags,likes,views,comments,downloads&per_page=40&page=${pageCounter}`
+    );
+    galleryItemsLimit = response.data.total;
+    console.log(galleryItemsLimit);
+    if (response.data.hits.length != 0) {
+      renderImages(response.data.hits);
+      if (pageCounter === 1) {
+        simpleGallery.refresh();
+        loadMoreVisible(true);
+        Notiflix.Notify.info(`Hooray! We found ${response.data.total} images.`);
+      }
+    } else {
+      loadMoreVisible(false);
+      return Notiflix.Notify.info(
+        '"Sorry, there are no images matching your search query. Please try again."'
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const newSearch = searchString => {
+  loadMoreVisible(false);
+  if (!isFirstSearch) {
+    gallery.innerHTML = '';
+    pageCounter = 1;
+    galleryItemsRendered = 0;
+  }
+  fetchImages(searchString);
+};
+
+const renderImages = data => {
+  isFirstSearch = false;
+  numbersOfPagesToRender -= 1;
+
+  const element = data
+    .map(elem => {
+      galleryItemsRendered += 1;
+
+      return `
+      <div class="photo-card__items">
+      <figure>
+       <a class="photo-card__item" href=${elem.largeImageURL}><img class="photo-card__image" src="${elem.webformatURL}" alt="" loading="lazy" /></a>
+      <figcaption class="info">
+     <p class="info-item">
+        <b><span>Likes:</span></b> <span> ${elem.likes}</span>
+      </p>
+     <p class="info-item">
+        <b><span>Views:</span></b><span>${elem.views}</span>
+     </p>
+     <p class="info-item">
+       <b><span>Comments:</span></b>${elem.comments}</span>
+      </p>
+      <p class="info-item">
+         <b><span>Downloads:</span></b><span>${elem.downloads}</span>
+    </p>
+    </figcaption>
+    </figure>
+    </div>
+</div>`;
+    })
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', element);
+  simpleGallery.refresh();
+  if (galleryItemsRendered === galleryItemsLimit) {
+    endReached();
+  }
+};
+const endReached = () => {
+  loadMoreVisible(false);
+  Notiflix.Notify.failure(
+    "We're sorry, but you've reached the end of search results."
+  );
+};
+loadMoreBtn.addEventListener('click', () => {
+  pageCounter += 1;
+  fetchImages(searchInput.value);
+});
+
+//Scroll Top button
+
+const scrollFunction = () => {
+  if (document.documentElement.scrollTop > 20) {
+    scrollTopBtn.style.display = 'block';
+  } else {
+    scrollTopBtn.style.display = 'none';
+  }
+};
+window.onscroll = () => {
+  scrollFunction();
+};
+
+const goTopFunction = () => {
+  document.documentElement.scrollTop = 0;
+};
+scrollTopBtn.addEventListener('click', () => {
+  goTopFunction();
+});
+
+//  Infinite Scroll
+
+const loadmore = () => {
+  pageCounter += 1;
+  fetchImages(searchInput.value);
+};
+
+const handleScroll = autoScrollFlag => {
+  if (autoScrollFlag) {
+    const endOfPage =
+      window.innerHeight + window.pageYOffset >= document.body.offsetHeight;
+    if (endOfPage && galleryItemsRendered < galleryItemsLimit) loadmore(); // sprawdzam czy znalazłam się na koncu strony oraz czy wyrenderowane zostały wszystkie obrazy
+  }
+};
+
+window.addEventListener(
+  'scroll',
+  throttle(250, () => {
+    handleScroll(autoScrollFlag);
+  })
+);
+
+// dynamiczne przełącznie pomiędzy infinite scroll a on demand
+
+const loadFormBtnAction = () => {
+  const checkValue = () => {
+    if (loadFormBtn.innerText === 'Auto load') {
+      loadMoreVisiblecheck = true;
+      loadMoreVisible(true);
+      autoScrollFlag = false;
+      return 'On demand';
+    }
+    if (loadFormBtn.innerText === 'On demand') {
+      loadMoreVisible(false);
+      autoScrollFlag = true;
+      return 'Auto load';
+    }
+  };
+  loadFormBtn.innerText = checkValue();
+};
+
+loadFormBtn.addEventListener('click', () => {
+  loadFormBtnAction();
+});
